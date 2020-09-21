1b7cb9ffe1d5c95d1ad123e1de128f593db889e7
Falko Schumann <falko.schumann@muspellheim.de>
2020-09-21 22:16:43 +0200

    Init project structure

---
+++ kanbanboard/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    application
+}
+
+dependencies {
+    implementation(project(":kanbanboard-backend"))
+    implementation(project(":kanbanboard-frontend"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClassName = "de.muspellheim.kanbanboard.AppKt"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "kanbanboard-root"
+include("kanbanboard")
+include("kanbanboard-contract")
+include("kanbanboard-backend")
+include("kanbanboard-frontend")
